f1044b17463e97ae711f094f0128a838740d9f6d
Evans K. Nyamekye <nyamekye2131@gmail.com>
2023-11-09 09:49:46 -0800

    Update: fixed all issues in OOP school library: set up associations

--- classes/person.rb
+++ classes/person.rb
@@ -3,7 +3,7 @@ require_relative 'capitalize_decorator'
 require_relative 'trimmer_decorator'
 
 class Person < Nameable
-  attr_accessor :title, :author, :rental
+  attr_accessor :name, :age, parent_permission, :rentals
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
--- classes/student.rb
+++ classes/student.rb
@@ -3,11 +3,14 @@ require './person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, name = 'Unknown')
-    super(age, name)
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @classroom = classroom
   end
 
   def classroom=(classroom)
+    @classroom&.students&.delete(self)
+
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
