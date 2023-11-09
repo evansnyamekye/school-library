5f4665e52c0c905c168b8385274b1d3097445cd9
Evans K. Nyamekye <nyamekye2131@gmail.com>
2023-11-08 23:00:01 -0800

    OOP school library: decorate a class

--- classes/base_decorator.rb
+++ classes/base_decorator.rb
@@ -6,7 +6,7 @@ class BaseDecorator < Nameable
     @nameable = nameable
   end
 
-  def correct_name
+  def correct_name 
     @nameable.correct_name
   end
 end
--- classes/nameable.rb
+++ classes/nameable.rb
@@ -2,4 +2,4 @@ class Nameable
   def correct_name
     raise NotImplementedError, 'This method is not yet implemented'
   end
-end
+end 
