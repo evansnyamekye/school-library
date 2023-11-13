3f673c68b4c7bd6c2ba5ed0eb885946eb0e5edf6
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-11-13 16:44:56 +0300

    Add options method

--- classes/main.rb
+++ classes/main.rb
@@ -31,4 +31,16 @@ def display_menu
   puts 'Enter number: '
 end
 
+def options(option, app)
+  case option
+  when '1' then app.book_list
+  when '2' then app.people_list
+  when '3' then app.person_create
+  when '4' then app.new_book
+  when '5' then app.new_rental
+  when '6' then app.rental_list
+  else
+    puts 'This is not a valid option'
+  end
+end
 main
